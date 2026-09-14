157fe2a2ec31ac58f4078c371bb27d8bafe17d4a
t-miyagawa61 <miyagawa-t@near-email.co.jp>
2026-09-14 11:43:28 +0900

    JS更新

--- sample/JS/UnitySendMessage.js
+++ sample/JS/UnitySendMessage.js
@@ -3,4 +3,12 @@ function sendDataToUnity() {
     
     // 引数：( "オブジェクト名", "関数名", "渡したい文字列" )
     unityInstance.SendMessage('SampleAnimetion_0', 'PlayAnimation', data);
+    
+    
+    location.href = "result.html";
 }
+
+let button = document.getElementById("button");
+button.addEventListener("click",sendDataToUnity);
+
+
