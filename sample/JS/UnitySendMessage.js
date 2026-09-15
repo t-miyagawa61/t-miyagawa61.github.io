f70d03d4453c8c4119e3af85d6956932aa13e79a
t-miyagawa61 <miyagawa-t@near-email.co.jp>
2026-09-15 12:00:23 +0900

    Update UnitySendMessage.js

--- sample/JS/UnitySendMessage.js
+++ sample/JS/UnitySendMessage.js
@@ -31,8 +31,8 @@ button.addEventListener("click",sendDataToUnity);
 // Unityから呼び出される関数をあらかじめ定義しておく
 function showResult(status) {
   //console.log('showResultを実行');
-  //document.body.style.setProperty('display', 'none', 'important');
-  //document.body.style.visibility = 'hidden';
+  document.body.style.setProperty('display', 'none', 'important');
+  document.body.style.visibility = 'hidden';
   //location.href = "result.html";
 }
   
